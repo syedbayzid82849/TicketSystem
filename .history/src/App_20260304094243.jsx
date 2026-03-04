a8b2ef2769896db791b6427f5936c3c5905574d7
syed bayzid <husains82849@gmail.com>
2026-03-04 09:52:15 +0600

    first commit

---
+++ .history/src/App_20260304094243.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Nav
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
---
+++ .history/src/App_20260304094246.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './Components/Navbar'
+
+function App() {
+
+  return (
+    <>
+    <Navbar
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
