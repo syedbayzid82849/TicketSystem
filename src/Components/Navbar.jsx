2f0fd662cda9a8b4ece2b23e8c74c3f4a61e0c50
syed bayzid <husains82849@gmail.com>
2026-03-04 11:24:22 +0600

    navbar and toast alert added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,18 @@
+import { toast } from "react-toastify";
 
 const Navbar = () => {
+    const onNewTicket = () => {
+        toast("Ticket Added to In Progress!");
+    }
+    // ["Home", "", "Changelog", "Blog", "Download", "Contact"]
+    const navLinks = <>
+        <li><a>Home</a></li>
+        <li><a>FAQ</a></li>
+        <li><a>Changelog</a></li>
+        <li><a>Blog</a></li>
+        <li><a>Contact</a></li>
+    </>;
+
     return (
         <nav className="navbar bg-base-100 shadow-sm">
             <div className="navbar-start">
@@ -10,36 +23,36 @@ const Navbar = () => {
                     <ul
                         tabIndex="-1"
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {navLinks}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
-            </div>
+                {/* Logo */}
+                <div
+                    style={{ fontWeight: 800, fontSize: 18, color: "#111827", letterSpacing: -0.5 }}
+                >
+                    Ticket System
+                </div>            </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2 bg-base-100 w-40 z-1">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    {navLinks}
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <button
+                    onClick={onNewTicket}
+                    style={{
+                        background: "linear-gradient(135deg, #6366f1, #8b5cf6)",
+                        color: "#fff",
+                        border: "none",
+                        borderRadius: 8,
+                        padding: "10px",
+                        fontWeight: 700,
+                        fontSize: 14,
+                        cursor: "pointer",
+                    }}
+                >
+                    + New Ticket
+                </button>
             </div>
         </nav>
     );
